5972d9078cc12226547c8c7a8bde3fb2c771df97
NeilHunterr <neilhunter40@gmail.com>
2020-12-08 17:18:18 +0000

    Add Script for contact form and Update contact us html

--- Scripts/PHP/ContactUsProcessForm.php
+++ Scripts/PHP/ContactUsProcessForm.php
@@ -1,1 +1,63 @@
+
+<!--
+file: ContactUsProcessForm.php
+author: Neil Hunter
+date: 08/12/2020
+desc: Create the contact us form processing
+-->
+
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jobSearchUK";
+$conn = "";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "Connected successfully <br />";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$query = $conn->prepare(
+    "INSERT INTO contacts (forename, surname, email, message)
+    VALUES (?, ?, ?, ?)"
+);
+
+$query->bindParam(1, $forename);
+$query->bindParam(2, $surname);
+$query->bindParam(3, $email);
+$query->bindParam(4, $message);
+
+$forename = $_POST['forename'];
+$surname = $_POST['surname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$query->execute();
+$conn = null;
+
+echo <<<EOL
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Us Confirmation</title>
+    <link href="../Styles/MyCSS.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<main>
+    <h2>Thank you, $forename $surname.</h2>
+    <br>
+    <p>The following message has been received:</p>
+    <br>
+    <p id="confirm-message">$message</p>
+    <br>
+    <p id="confirm-note">Please allow for 12-48 hours for a response.</p>
+</main>
+
+</body>
+</html>
+EOL;
