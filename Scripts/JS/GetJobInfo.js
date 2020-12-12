dfab73a9101844e70d175b6871fda2f87e5c021f
NeilHunterr <neilhunter40@gmail.com>
2020-12-12 19:48:44 +0000

    Update job info script and html

--- Scripts/JS/GetJobInfo.js
+++ Scripts/JS/GetJobInfo.js
@@ -1,3 +1,10 @@
+<!--
+file: GetJobInfo.js
+author: Neil Hunter
+date: 11/12/2020
+desc: job info js code
+-->
+
 
 export const getSocFromUrl = () => {
     const urlQueryString = window.location.search
@@ -5,7 +12,45 @@ export const getSocFromUrl = () => {
     return urlParameters.get('soc')
 }
 
- export const getJobCode = () => {
+export const getJobInfo = () => {
+
     const requestUrl = `http://api.lmiforall.org.uk/api/v1/soc/code/${getSocFromUrl()}`
 
- }
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('jobTitle').innerText = data.title
+            document.getElementById('socText').innerText = `SOC code: ${data.soc}`
+            document.getElementById('description').innerText = data.description
+
+            document.getElementById('qual').innerText = 'Qualifications'
+            document.getElementById('qualDesc').innerText = data.qualifications
+
+            document.getElementById('tasksTitle').innerText = 'Tasks'
+
+            // // Divide tasks into individual list items
+            // // TODO: Stop loop from appending empty children
+            // const tasksList = document.getElementById('tasksList')
+            // let subArray = []
+            // const regex = /^[A-Za-z\s]+$/;  // only match letters and spaces
+            // [...data.tasks].forEach(character => {
+            //     if (character.match(regex)) {
+            //         subArray.push(character)
+            //     } else {
+            //         const listItem = document.createElement('li')
+            //         listItem.innerHTML = subArray.join('')
+            //         tasksList.appendChild(listItem)
+            //         subArray = []
+            //     }
+            // })
+
+            document.getElementById('tagsTitle').innerText = 'Tags'
+            document.getElementById('tagsDesc').innerText = `[${data.add_titles.slice(0, 10)}]`
+        })
+        .catch(error => console.log(`response error: ${error}`))
+}
+
+// document on ready
+$(function () {
+    getJobInfo()
+})
